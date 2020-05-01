43a76310bc1a03751a3e5b275ae5734ebcf00442
Elahe-rb <rabbani.elahe@gmail.com>
2020-05-01 21:39:41 +0430

    simple smn

---
+++ preprocess_data_smn.py
@@ -0,0 +1,310 @@
+import torch
+import re
+import csv
+import nltk
+import random
+import math
+from torch.autograd import Variable
+from nltk.stem import SnowballStemmer
+
+######################################################################
+# Load & Preprocess Data
+# ---
+# Default word tokens
+PAD_token = 0  # Used for padding short sentences
+EOU_token = 1  # end-of-utt token
+EOT_token = 2  # End-of-turn token
+
+class Voc:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False
+        self.word2count = {}  #what about <UNK> ?  #without trimming
+        self.num_words = 0  # Count SOS, EOS, PAD  #without trimming
+        self.unique_vocabs = []
+        self.contexts_word_to_df = {}
+        self.responses_word_to_df = {}
+        self.num_cr_pairs_docs = 0
+
+    def addSentence(self, sentence, is_context, is_response):
+        words_in_sentece = []
+        for word in sentence.split(' '):    #or nltk tokenize
+            self.addWord(word)
+            if word not in words_in_sentece:
+                words_in_sentece.append(word)
+        if is_context:
+            self.num_cr_pairs_docs += 1
+            for w in words_in_sentece:
+                if w not in self.contexts_word_to_df:
+                    self.contexts_word_to_df[w] = 1
+                else:
+                    self.contexts_word_to_df[w] += 1
+        if is_response:
+            for w in words_in_sentece:
+                if w not in self.responses_word_to_df:
+                    self.responses_word_to_df[w] = 1
+                else:
+                    self.responses_word_to_df[w] += 1
+
+    def addWord(self, word):
+        if word not in self.word2count:
+            self.word2count[word] = 1
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+
+    # Remove words below a certain count threshold
+    def trim(self, min_count):
+        if self.trimmed:
+            return
+        self.trimmed = True
+
+        keep_words = []
+
+        keep_words.append("<UNK>")
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+
+        print('keep_words {} / {} = {:.4f}'.format(
+            len(keep_words), len(self.word2count), len(keep_words) / len(self.word2count)
+        ))
+
+        # Reinitialize dictionaries
+        #self.word2count = {} #{"SOS": 1000, "EOS": 1000}
+        #self.num_words = 0  # Count default tokens
+
+        self.unique_vocabs = keep_words
+        #for word in keep_words:
+        # #self.addWord(word)
+
+##########################################
+
+
+# Lowercase, trim, and remove non-letter characters
+def normalizeString(s, stemmer):
+    #s = ' '.join(list(map(stemmer.stem, nltk.word_tokenize(str))))
+    s = s.lower().strip()
+    s = re.sub(r"([.!\?\\/+*:&$%#@~=,\-\)\(])", r" \1 ", s)
+    s = re.sub(r"[^a-zA-Z0-9'!\?]", r" ", s)
+    s = re.sub(r"\s+", r" ", s).strip()
+    return s
+
+def clean_data(rows):
+    #?? add tokenization lemma and stem
+    normalized_rows = []
+    stemmer = SnowballStemmer("english")
+    for row in rows:
+        normalized_row = [normalizeString(r, stemmer) for r in row]
+        normalized_rows.append(normalized_row)
+    return normalized_rows
+
+def readFile(filepath):
+
+    reader = csv.reader(open(filepath), delimiter="\t")
+    rows = list(reader)[0:20]
+    rows = clean_data(rows)  #if uncomment change _eot_ to eot in numberize function
+    return rows
+
+def readUidsFile(filepath):
+    reader = csv.reader(open(filepath), delimiter="\t")
+    rows = list(reader)[0:20]
+    return rows
+
+#iter starts from 0
+def get_batch(rows,iter,batch_size):
+    start = iter * batch_size
+    return rows[start: start+batch_size]
+
+def load_vocab(filename):
+  lines = open(filename).readlines()
+  return {
+    word.strip() : i+1
+    for i,word in enumerate(lines)
+  }
+
+def build_vocab(corpus_name, train_data, valid_data, vocab_path, trim):
+
+    voc = Voc(corpus_name)
+
+    i = 0
+    for text in train_data:
+        if text[0] == '1':
+            context = ''
+            for j in range(1, len(text) - 1):
+                context = context + text[j] + " eot "
+            voc.addSentence(context, True, False)
+            voc.addSentence(text[-1] + " eot ", False, True)  # for context data it contains only context response pairs with label 1
+            i += 1
+
+    for text in valid_data:
+        if text[0] == '1':
+            context = ''
+            for j in range(1, len(text) - 1):
+                context = context + text[j] + " eot "
+            voc.addSentence(context, True, False)
+            voc.addSentence(text[-1] + " eot ", False, True)  #for valid data it contains only ground response
+            i += 1
+    if trim:
+        voc.trim(8)
+
+    with open(vocab_path, 'w') as f:
+        f.writelines("%s\n" % w for w in voc.unique_vocabs)
+    f.close()
+
+    return voc
+
+def load_glove_embeddings(vocab, filename='../data/glove.6B.200d.txt'):
+    lines = open(filename).readlines()
+    embeddings = {}
+    not_oov = 0
+    for line in lines:
+        word = line.split()[0]
+        embedding = list(map(float, line.split()[1:]))
+        if word in vocab:
+            embeddings[vocab[word]] = embedding
+            not_oov = not_oov + 1
+    print(len(vocab)-not_oov)
+    return embeddings
+
+def numberize(inp, vocab, max_utt_num , max_utt_length, dic, is_context):
+    #max_len = max_utt_num * max_utt_length
+    if is_context:
+        nested_inp = inp.split('eot')[:-1]   #-1 is for ignoring the last one which is empty #change to eot for run in cuda and uncomment clean_data
+        #nested_inp = [item for item in nested_inp if len(item)>2]
+        selected_turns = nested_inp[-min(max_utt_num, len(nested_inp)):]
+        selected_words_in_turns = [words.split()[:min(len(words),max_utt_length)] for words in selected_turns]
+
+        padded_nested_results = []
+        PAD_SEQUENCE = [0] * max_utt_length
+        for turn_sequence in selected_words_in_turns:
+            selected_context = list(map(lambda k: vocab.get(k, 1), turn_sequence[:]))
+            if(len(selected_context)<max_utt_length):
+                selected_context += [0] * (max_utt_length - len(selected_context))
+            padded_nested_results.append(selected_context)
+        padd_len = len(padded_nested_results)
+        if(padd_len<max_utt_num):
+            padded_nested_results += [PAD_SEQUENCE] * (max_utt_num - padd_len)
+        final_seq = padded_nested_results
+        #selected_context = [w for ut in selected_words_in_turns for w in ut]
+        #df_features =  list(map(lambda k: dic.contexts_word_to_df.get(k, 1), selected_context))  #it should be .get(k,0) for full data
+
+
+    else:
+        words = inp.split()
+        selected_context = words[:min(len(words), max_utt_length)]
+        result = list(map(lambda k: vocab.get(k, 1), selected_context))  # [-max_length:]   # 1 is index of unkown words
+        pad_first_size = max_utt_length - len(result)
+        if len(result) < max_utt_length:
+            #result = [0] * pad_first_size + result
+            result = result + [0] * pad_first_size   #post padding as is used in smn implementation of paper
+        if len(result) != max_utt_length:
+            print('errrrrorrr')
+
+        final_seq = result
+        #df_features = list(map(lambda k: dic.contexts_word_to_df.get(k, 1), selected_context))
+
+    #tf_features = list(map(lambda k: (selected_context.count(k)/len(selected_context)) , selected_context))
+    #idf_features = [math.log(dic.num_cr_pairs_docs / x) for x in df_features]
+    #result = list(map(lambda k: vocab.get(k, 1), selected_context))#[-max_length:]   # 1 is index of unkown words
+
+
+
+    #result dim: seqlength ===> unsquueze(1) :seq*1
+    #result = torch.cat((torch.FloatTensor(result).unsqueeze(1),torch.FloatTensor(idf_features).unsqueeze(1), torch.FloatTensor(tf_features).unsqueeze(1)),1)
+    return final_seq   #dim seq*(numOffeatures+1)  here is 2(0:word indx, 1:idf, 2:tf)
+
+def process_predict_embed(response):
+    stemmer = SnowballStemmer("english")
+    response = ' '.join(list(map(stemmer.stem, nltk.word_tokenize(response))))
+    response = numberize(response)
+    return response
+
+def process_train_data(rows, batch, batch_size, vocab, max_utt_num, max_utt_length, dic, device, is_topNet, uids_rows, cluster_ids):
+    count = 0
+    cs = []
+    rs = []
+    ys = []
+    contexts = []
+    clusters = []
+
+    batched_rows = get_batch(rows, batch, batch_size)
+    batched_uids_rows = get_batch(uids_rows, batch, batch_size)
+
+    #max_length_context = max(len(row[0]) for row in batched_rows )
+    #max_length_response = max(len(row[1]) for row in batched_rows )
+
+    for (row, rowuid) in zip(batched_rows, batched_uids_rows):
+
+        label = row[0]
+        context = ''
+        for i in range(1, len(row) - 1):
+            context = context + row[i] + " eot "
+        response = row[-1] + " eot "
+
+        if is_topNet:
+            response_user_id = rowuid[0].split(",")[-1].split("-")[1]
+            cluster = cluster_ids.get(response_user_id)
+
+        context = numberize(context, vocab, max_utt_num, max_utt_length, dic, True)  #dim: context: seq*num_of_features
+        response = numberize(response, vocab, max_utt_num, max_utt_length, dic, False) #dim: response: seq*num_of_features
+        label = int(label)
+        count += 1
+        cs.append(torch.LongTensor(context))
+        rs.append(torch.LongTensor(response))
+        ys.append(torch.FloatTensor([label]))
+        contexts.append(context)
+        if is_topNet:
+            clusters.append(cluster)
+
+    cs = torch.stack(cs, 0).to(device)  # dim: batchsize * max-utt-num * max_utt-length
+    rs = torch.stack(rs, 0).to(device)  #dim: batchsize * max_utt_num
+    ys = torch.stack(ys, 0).to(device)
+
+    if is_topNet:
+        return cs, rs, ys, clusters
+
+    return cs, rs, ys
+
+def process_valid_data(rows, batch, batch_size, vocab, max_utt_num, max_utt_length, dic, device):
+
+    batched_cs = []
+    batched_rs = [] #contains ground response and its corresponding distractors
+
+    batched_rows = get_batch(rows, batch, batch_size)
+    for row in batched_rows:
+
+        context = row[0]
+        correct_response = row[1]
+        distractors = row[2:]
+        temp_dis = []
+
+        context = numberize(context, vocab, max_utt_num, max_utt_length, dic, True)
+        correct_response = numberize(correct_response, vocab, max_utt_num, max_utt_length, dic, False)
+        distractors = [numberize(distractor, vocab, max_utt_num, max_utt_length, dic, False) for distractor in distractors] #dim: 9*seq*numFeatures
+
+        with torch.no_grad():
+            cs = torch.stack([context for i in range(10)], 0).to(device)   #10*seq*numF
+            rs = [correct_response]
+            rs += [distractor for distractor in distractors]
+            rs = torch.stack(rs, 0).to(device)      #dim: 10*seq*numF
+
+            batched_cs.append(cs)
+            batched_rs.append(rs)
+
+    return batched_cs, batched_rs   #b*10*seq*numF
+
+def load_Data(train_path, valid_path, test_path, vocab_path, train_uids_path, valid_uids_path, test_uids_path,):
+    train = readFile(train_path)
+    train_uids = readUidsFile(train_uids_path)
+    train_data = list(zip(train, train_uids))
+    #random.shuffle(train_data)
+    train, train_uids = zip(*train_data)
+    valid = readFile(valid_path)
+    valid_uids = readUidsFile(valid_uids_path)
+    test = readFile(test_path)
+    test_uids = readUidsFile(test_uids_path)
+    #to build vocabulary.txt
+    dic = build_vocab('myVocab', train , valid, vocab_path, trim = True)
+    vocab = load_vocab(vocab_path)
+    return train, valid, test, vocab, dic, train_uids, valid_uids, test_uids
---
+++ smn.py
@@ -0,0 +1,271 @@
+import datetime
+import time
+import torch
+from torch import nn
+from torch.nn import init
+from torch.autograd import Variable
+import torch.nn.functional as F
+import preprocess_data_smn
+import numpy as np
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+# SMN from sequential matching network
+class SMN(nn.Module):
+    def __init__(
+            self,
+            vocab,
+            input_size,
+            hidden_size,
+            vocab_size,
+            bidirectional,
+            rnn_type,
+            num_layers,
+            dropout,
+    ):
+        super(SMN, self).__init__()
+        self.num_directions = 2 if bidirectional else 1
+        self.vocab = vocab
+        self.vocab_size = len(vocab) + 1  # +1 is for padding words
+        self.emb_h_size = input_size  # embedding dim
+        self.rnn_h_size = hidden_size  # rnn hidden dim
+        self.num_layers = num_layers
+        self.rnn_type = rnn_type
+        self.p_dropout = dropout
+
+        self.embedding = nn.Embedding(vocab_size, input_size, padding_idx=0)
+
+        M = torch.FloatTensor(self.rnn_h_size, self.rnn_h_size).to(device)
+        init.normal_(M)
+        self.M = nn.Parameter(M, requires_grad=True)
+
+        W = torch.FloatTensor(self.rnn_h_size, self.rnn_h_size).to(device)
+        init.normal_(W)
+        self.W = nn.Parameter(W, requires_grad=True)
+
+        M1 = torch.FloatTensor(6, 1).to(device)
+        init.normal_(M1)
+        self.M1 = nn.Parameter(M1, requires_grad=True)
+
+        M2 = torch.FloatTensor(6, 1).to(device)
+        init.normal_(M2)
+        self.M2 = nn.Parameter(M2, requires_grad=True)
+
+        self.dropout_layer = nn.Dropout(self.p_dropout)
+
+
+        ###########################################################################################################
+        self.utterance_GRU = nn.GRU(self.emb_h_size, self.rnn_h_size, bidirectional=bidirectional, batch_first=True, dropout=0).to(device)
+        # ih_u = (param.data for name, param in self.utterance_GRU.named_parameters() if 'weight_ih' in name)
+        # hh_u = (param.data for name, param in self.utterance_GRU.named_parameters() if 'weight_hh' in name)
+        # for k in ih_u:
+        #     nn.init.orthogonal_(k)
+        # for k in hh_u:
+        #     nn.init.orthogonal_(k)
+        # 用于response的GRU
+        self.response_GRU = nn.GRU(self.emb_h_size, self.rnn_h_size, bidirectional=bidirectional, batch_first=True, dropout=0).to(device)
+        # ih_r = (param.data for name, param in self.response_GRU.named_parameters() if 'weight_ih' in name)
+        # hh_r = (param.data for name, param in self.response_GRU.named_parameters() if 'weight_hh' in name)
+        # for k in ih_r:
+        #     nn.init.orthogonal_(k)
+        # for k in hh_r:
+        #     nn.init.orthogonal_(k)
+
+        self.conv2d = nn.Conv2d(2, 8, kernel_size=(3, 3))
+        conv2d_weight = (param.data for name, param in self.conv2d.named_parameters() if "weight" in name)
+        for w in conv2d_weight:
+            init.kaiming_normal_(w)
+
+        self.pool2d = nn.MaxPool2d((3, 3), stride=(3, 3))
+
+        self.linear = nn.Linear(16 * 16 * 8, 50)
+        linear_weight = (param.data for name, param in self.linear.named_parameters() if "weight" in name)
+        for w in linear_weight:
+            init.xavier_uniform_(w)
+
+        self.Amatrix = torch.ones((self.rnn_h_size, self.rnn_h_size), requires_grad=True)
+        init.xavier_uniform_(self.Amatrix)
+        self.Amatrix = self.Amatrix.to(device)
+
+        self.final_GRU = nn.GRU(50, self.rnn_h_size, bidirectional=False, batch_first=True)
+        ih_f = (param.data for name, param in self.final_GRU.named_parameters() if 'weight_ih' in name)
+        hh_f = (param.data for name, param in self.final_GRU.named_parameters() if 'weight_hh' in name)
+        for k in ih_f:
+            nn.init.orthogonal_(k)
+        for k in hh_f:
+            nn.init.orthogonal_(k)
+
+        self.final_linear = nn.Linear(100, 1)
+        final_linear_weight = (param.data for name, param in self.final_linear.named_parameters() if "weight" in name)
+        for w in final_linear_weight:
+            init.xavier_uniform_(w)
+        ############################################################################################################
+
+        self.init_weights()
+
+    def init_weights(self):
+        init.uniform_(self.utterance_GRU.weight_ih_l0, a=-0.01, b=0.01)
+        init.orthogonal_(self.utterance_GRU.weight_hh_l0)
+        self.utterance_GRU.weight_ih_l0.requires_grad = True
+        self.utterance_GRU.weight_hh_l0.requires_grad = True
+
+        init.uniform_(self.response_GRU.weight_ih_l0, a=-0.01, b=0.01)
+        init.orthogonal_(self.response_GRU.weight_hh_l0)
+        self.response_GRU.weight_ih_l0.requires_grad = True
+        self.response_GRU.weight_hh_l0.requires_grad = True
+
+        glove_embeddings = preprocess_data_smn.load_glove_embeddings(self.vocab)
+
+        embedding_weights = torch.FloatTensor(self.vocab_size, self.emb_h_size)
+        init.uniform_(embedding_weights, a=-0.25, b=0.25)
+        for k, v in glove_embeddings.items():
+            embedding_weights[k] = torch.FloatTensor(v)
+        embedding_weights[0] = torch.FloatTensor([0] * self.emb_h_size)
+
+        self.embedding.weight = nn.Parameter(embedding_weights, requires_grad=False)
+
+    def forward(self, utterance, response):
+        '''
+            utterance:(self.batch_size, self.max_num_utterance, self.max_sentence_len)
+            response:(self.batch_size, self.max_sentence_len)
+        '''
+        #uttereance: (batch_size,10(uttNum),50(uttlength))-->(batch_size,10,50,200)
+        #response:(batch_size,50)-->(batch_size,50,200)
+        all_utterance_embeddings = self.embedding(utterance)
+        response_embeddings = self.embedding(response)
+
+        # tensorflow:(batch_size,10,50,200)-->分解-->10个array(batch_size,50,200)
+        # pytorch:(batch_size,10,50,200)-->(10,batch_size,50,200)
+        all_utterance_embeddings = all_utterance_embeddings.permute(1, 0, 2, 3)
+
+        # 先处理response的gru
+        response_GRU_embeddings, _ = self.response_GRU(response_embeddings)
+        response_embeddings = response_embeddings.permute(0, 2, 1)
+        response_GRU_embeddings = response_GRU_embeddings.permute(0, 2, 1)
+        matching_vectors = []
+
+        for utterance_embeddings in all_utterance_embeddings:
+            matrix1 = torch.matmul(utterance_embeddings, response_embeddings)  # batch*utlen*utlen<-- batch*uttlength*embdim   , batch*embdim*uttlength
+
+            utterance_GRU_embeddings, _ = self.utterance_GRU(utterance_embeddings)
+            matrix2 = torch.einsum('aij,jk->aik', [utterance_GRU_embeddings, self.Amatrix])
+            matrix2 = torch.matmul(matrix2, response_GRU_embeddings)
+
+            matrix = torch.stack([matrix1, matrix2], dim=1)
+            # matrix:(batch_size,channel,seq_len,embedding_size)
+            conv_layer = self.conv2d(matrix)
+            # add activate function
+            conv_layer = F.relu(conv_layer)
+            pooling_layer = self.pool2d(conv_layer)
+            # flatten
+            pooling_layer = pooling_layer.view(pooling_layer.size(0), -1)
+            matching_vector = self.linear(pooling_layer)
+            # add activate function
+            matching_vector = torch.tanh(matching_vector)
+            matching_vectors.append(matching_vector)
+
+        _, last_hidden = self.final_GRU(torch.stack(matching_vectors, dim=1))
+        last_hidden = torch.squeeze(last_hidden)
+        logits = self.final_linear(last_hidden)
+
+        # use CrossEntropyLoss,this loss function would accumulate softmax
+        #y_pred = F.softmax(logits)    #logit dim: [batchsize,1]
+        #y_pred = logits)
+        y_pred = torch.sigmoid(logits)
+        #predddd = F.softmax(logits,dim=0)
+        return y_pred     #dim: batchsize*1
+
+    '''
+    def forward(self, contexts_features, responses_features, scores, data_clusters):  # dim:b*seq*num_of_features (0:word_indx 1:df)   scores: dim: b   :is a vector of batch size
+        # print("hello, in dual encoder in top_network dir!!")
+        contexts = contexts_features[:, :, 0].long()  # dim: b*seq
+        responses = responses_features[:, :, 0].long()
+        c_idf = contexts_features[:, :, 1]  # dim: b*seq
+        r_idf = responses_features[:, :, 1]
+        c_tf = contexts_features[:, :, 2]
+        r_tf = responses_features[:, :, 2]
+        c_tf_idf = torch.mul(c_idf, c_tf)  # element wise multiplication dim: b*seq <== a*b
+        r_tf_idf = torch.mul(r_idf, r_tf)  # element wise multiplication dim: b*seq <== a*b
+        results = self.att_on_embedding(contexts, responses, c_tf_idf, r_tf_idf, scores.to(device),
+                                        data_clusters.to(device))
+        # results = self.dmn_prf(contexts,responses, c_tf_idf, r_tf_idf)
+
+        return results
+    '''
+
+    def attention(self, att_context, query, tf_idf):  # dim: (att_context): b*seq*hidden  (query):b*1*hidden
+        # energy = torch.bmm(att_context, torch.mm(query.squeeze(1), self.W).unsqueeze(2)) #energy: b*seq*1 <== b*seq*hidden , b*hidden*1
+        energy = torch.bmm(att_context, query.transpose(1, 2)).to(device)  # energy: b*seq*1 <== 512*500*300 , 512*300*1
+        # energy = torch.bmm(energy, tf_idf)
+        Attention_weights = F.softmax(energy, dim=1)  # b*seq*1
+        Attention_weights = torch.mul(Attention_weights, tf_idf.unsqueeze(2))
+        weighted_average = torch.bmm(att_context.transpose(1, 2), Attention_weights).to(
+            device)  # 512*300*1 <== 512*300*500 , 512*500*1
+        output = weighted_average.transpose(1, 2)  # 512*1*300
+        return output  # dim: b * 1 * hidden
+
+    def att_on_embedding(self, contexts, responses, c_tf_idf, r_tf_idf, scores,
+                         data_clusters):  # dim context:b*seq  response:b*seq
+
+        contexts_emb = self.embedding(contexts)  # dim: c
+        responses_emb = self.embedding(responses)
+
+        context_os, context_hs = self.rnn(
+            contexts_emb)  # context_hs dimensions: ( (numlayers*num direction) * batch_size * hidden_size)
+        response_os, response_hs = self.rnn(
+            responses_emb)  # context_os dimensions: (batch_size * seq_length * hidden_size)
+        context_hs = context_hs[
+            -1]  # dim: b*h   ::get the hidden of last layer(for single layer this is same as context_hs.squeeze(0))
+        response_hs = response_hs[
+            -1]  # dim: batch_size * hidden_size <=== (numlayers*num direction) * batch_size * hidden_size
+
+        ##multiply by idf
+        # c_df = c_df.float()
+        # r_df = r_df.float()
+        # context_emb_idf = torch.mul(contexts_emb, c_tf_idf.unsqueeze(2).expand_as(contexts_emb))      #dim    <== b*seq*emb , b*seq
+        # response_emb_idf = torch.mul(responses_emb, r_tf_idf.unsqueeze(2).expand_as(responses_emb))
+
+        #### attention:
+        # context_att = self.attention(response_emb_idf, context_hs.view(-1, 1, self.rnn_h_size))  # b*1*hidden
+        # response_att = self.attention(context_emb_idf, response_hs.view(-1, 1, self.rnn_h_size))  # b*1*hidden <==
+
+        # context_att = self.attention(responses_emb, context_hs.view(-1, 1, self.rnn_h_size), r_tf_idf)  # b*1*hidden
+        # response_att = self.attention(contexts_emb, response_hs.view(-1, 1, self.rnn_h_size), c_tf_idf)  # b*1*hidden <==
+
+        ### concat with gru encoding
+        # context_concat = torch.cat((context_hs.unsqueeze(1), context_att), 2)  # b*1*(h+h)
+        # response_concat = torch.cat((response_hs.unsqueeze(1), response_att), 2)  # b*1*(h+h)
+        #
+        # context_enc = context_concat
+        # response_enc = response_concat
+        # response_enc = response_att
+        # context_enc = context_att
+
+        ##here needs context_enc and response_enc with dim: b* 1 * h
+        # context_enc = context_enc.squeeze(1)  # b*h <== b*1*h
+        # context_rep = context_enc.mm(self.M).to(device)  # dimensions: (batch_size x hidden_size)  <== (b*h) , (h*h)
+        # context_rep = context_rep.unsqueeze(1)  # dimensions: (batch_size x 1 x hidden_size)
+        # response_rep = response_enc.transpose(1, 2)  # dimensions: (batch_size x hidden_size x 1)
+
+        ### simple dual gru
+        # context = context_hs.mm(self.M).to(device)  # dimensions: (batch_size x hidden_size)  <== (b*h) , (h*h)
+        # context = context.view(-1, 1, self.rnn_h_size)  # dimensions: (batch_size x 1 x hidden_size)
+        # response = response_hs.view(-1, self.rnn_h_size, 1)  # dimensions: (batch_size x hidden_size x 1)
+
+        ### simple dual gru
+        context = context_hs.mm(self.M).to(device)  # dimensions: (batch_size x hidden_size)  <== (b*h) , (h*h)
+        context_rep = context.view(-1, 1, self.rnn_h_size)  # dimensions: (batch_size x 1 x hidden_size)
+        response_rep = response_hs.view(-1, self.rnn_h_size, 1)  # dimensions: (batch_size x hidden_size x 1)
+        ans = torch.bmm(context_rep, response_rep).view(-1, 1).to(
+            device)  # dimensions: (batch_size x 1 x 1) and lastly --> (batch_size x 1)
+
+        scores = scores.view(-1, 1)  # convert b ---> b*1
+        n_scores = scores * data_clusters
+        n_ans = ans * data_clusters
+        res = (n_ans.mm(self.M1).to(device)) + (n_scores.mm(self.M2).to(device))
+
+        results = torch.sigmoid(res)  # dim: batchsize * 1
+        return results
+
