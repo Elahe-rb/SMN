737f1c74ee2e817b8dbe7876f000b85c4628614f
Elahe-rb <rabbani.elahe@gmail.com>
2020-05-03 01:51:56 +0430

    simple smn

--- preprocess_data_smn.py
+++ preprocess_data_smn.py
@@ -102,14 +102,14 @@ def clean_data(rows):
 def readFile(filepath):
 
     reader = csv.reader(open(filepath), delimiter="\t")
-    rows = list(reader)[0:173600]
+    rows = list(reader)[0:1000]
     print(len(rows))
     rows = clean_data(rows)  #if uncomment change _eot_ to eot in numberize function
     return rows
 
 def readUidsFile(filepath):
     reader = csv.reader(open(filepath), delimiter="\t")
-    rows = list(reader)[0:173600]
+    rows = list(reader)[0:1000]
     return rows
 
 #iter starts from 0
