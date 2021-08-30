fcc723f87e054a4e0edef559e49995a63642c237
Elahe-rb <rabbani.elahe@gmail.com>
2021-08-30 15:44:39 +0430

    smn

--- config_params_smn.py
+++ config_params_smn.py
@@ -8,7 +8,7 @@ num_classes = 1
 
 #udc params
 
-batch_size = 512#128#512
+batch_size = 40#512#128#512
 evaluate_batch_size = 500#100#500
 embed_dim = 200  # embedding dim: this is the input_size in rnn function
 hidden_size = 200  # rnn dim hidden states
