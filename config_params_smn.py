2fb1f83675f48d2f5a7cda0d82978f78352b923f
Elahe-rb <rabbani.elahe@gmail.com>
2021-09-17 15:24:39 +0430

    smn

--- config_params_smn.py
+++ config_params_smn.py
@@ -8,28 +8,28 @@ num_classes = 1
 
 DO_ClEAN = False
 IS_SMN = True
-min_freq = 8  #10 for udc  #***** Tip ****:: 8 is ok for MSDialog
+min_freq = 10 #8  #10 for udc  #***** Tip ****:: 8 is ok for MSDialog
 
-dataset="MSDialog"#"UDC"
+dataset='UDC'#"MSDialog"#"UDC"
 model_name = 'SMN_TEST'#'Dual_GRU' #'SMN'   #or Dual_GRU
 
 
 #udc params
 
-# batch_size = 100#128#40#512#128#512
-# evaluate_batch_size = 500#100#500
-# embed_dim = 200  # embedding dim: this is the input_size in rnn function
-# hidden_size = 200  # rnn dim hidden states
-# kernel_size = 3  #for both convolutional and maxpool
-# kernel_num = 8
-# dropout_rate = 0.3
+batch_size = 100#128#40#512#128#512
+evaluate_batch_size = 500#100#500
+embed_dim = 200  # embedding dim: this is the input_size in rnn function
+hidden_size = 200  # rnn dim hidden states
+kernel_size = 3  #for both convolutional and maxpool
+kernel_num = 8
+dropout_rate = 0.3
 # #vocabulary_size = 95154  # vocab size :based on existing tensorflow code for this paper
-# max_utterance_length = 50
-#max_conv_utt_num = 10   # the number of utterances in each conversation; if<10 padd it with zero else the most 10 recent utterance is kept
+max_utterance_length = 50
+max_conv_utt_num = 10   # the number of utterances in each conversation; if<10 padd it with zero else the most 10 recent utterance is kept
 
 
 #MsDialog
-
+'''
 batch_size = 60#50
 evaluate_batch_size = 250
 embed_dim = 200  # embedding dim: this is the input_size in rnn function
@@ -41,4 +41,4 @@ dropout_rate = 0.2
 max_utterance_length = 50#90   #90 as in paper said    but also check 50
 max_conv_utt_num = 10
 
-
+'''
