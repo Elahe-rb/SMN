65594bf86b50fa196e5b4d77e11952f2773dc39a
Elahe-rb <rabbani.elahe@gmail.com>
2021-09-29 01:35:52 +0330

    smn

--- config_params_smn.py
+++ config_params_smn.py
@@ -6,16 +6,16 @@ adam_beta1 = 0.9    #these are the defult betas value for adam
 adam_beta2 = 0.999
 num_classes = 1
 
-DO_ClEAN = True
-IS_SMN = False
+DO_ClEAN = False
+IS_SMN = True
 min_freq = 10 #8  #10 for udc  #***** Tip ****:: 8 is ok for MSDialog
 
-dataset="UDC"#"UDC"
-model_name = 'Dual_GRU'#'Dual_GRU' #'SMN'   #or Dual_GRU
+dataset="MSDialog"#"UDC"
+model_name = 'SMN_PAPER'#'Dual_GRU' #'SMN'   #or Dual_GRU
 
 
 #udc params
-
+'''
 batch_size = 100#128#40#512#128#512
 evaluate_batch_size = 500#100#500
 embed_dim = 200  # embedding dim: this is the input_size in rnn function
@@ -27,10 +27,10 @@ dropout_rate = 0.3
 max_utterance_length = 50
 max_conv_utt_num = 10   # the number of utterances in each conversation; if<10 padd it with zero else the most 10 recent utterance is kept
 
-
-#MsDialog
 '''
-batch_size = 60#50
+#MsDialog
+
+batch_size = 50
 evaluate_batch_size = 250
 embed_dim = 200  # embedding dim: this is the input_size in rnn function
 hidden_size = 100  # rnn dim hidden states
@@ -41,4 +41,4 @@ dropout_rate = 0.2
 max_utterance_length = 50#90   #90 as in paper said    but also check 50
 max_conv_utt_num = 10
 
-'''
+
